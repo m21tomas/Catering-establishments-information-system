de6caea6b96fb6618359a139f3ee372e405e701d
m21tomas <m21tomas@gmail.com>
2024-09-23 23:57:06 +0300

    Add build-script batch script

---
+++ build-script.bat
@@ -0,0 +1,53 @@
+@echo off
+SETLOCAL
+
+echo Removing old resources...
+cd Maitinimas-back\src\main\resources\public
+del /Q *.*
+
+echo Building react app...
+cd ..\..\..\..\..\Maitinimas-front\
+
+echo Checking Node.js version...
+node -v
+IF ERRORLEVEL 1 (
+    echo Node.js is not installed or not found in PATH. Exiting...
+    exit /B 1
+)
+
+echo Checking npm version...
+npm -v
+IF ERRORLEVEL 1 (
+    echo npm is not installed or not found in PATH. Exiting...
+    exit /B 1
+)
+
+echo Installing dependencies...
+npm install
+IF ERRORLEVEL 1 (
+    echo Npm install failed. Exiting...
+    exit /B 1
+)
+
+npm run build
+IF ERRORLEVEL 1 (
+    echo Npm build failed. Exiting...
+    exit /B 1
+) ELSE (
+    echo Npm build completed successfully.
+)
+
+echo Copying build to target...
+cd ..\Maitinimas-back\src\main\resources\public\
+xcopy /E /Y ..\..\..\..\..\Maitinimas-front\build\* .\
+
+echo Maven clean package...
+cd ..\..\..\..
+mvn clean package
+IF ERRORLEVEL 1 (
+    echo Maven clean package failed. Exiting...
+    exit /B 1
+)
+
+echo Build script completed successfully.
+ENDLOCAL
